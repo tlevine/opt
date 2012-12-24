9a4413ad022dfa3caf528ba5db494dc4ff0be03c
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 15:39:32 -0500

    gitmarks_2

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+sm () {
+  git submodule init && git submodule update
+}
 ln -s ~/opt/pdf2svg/pdf2svg.sh ~/bin/pdf2svg
 ln -s ~/opt/spark/spark ~/bin/spark
 ln -s ~/opt/urchin/urchin ~/bin/urchin
+
+sm
+(
+  cd gitmarks_2 && git checkout tlevine && sm 
+)
+
 echo 'Source ~/opt/nvm/nvm.sh in your profile.'
+echo 'Put your gitmarks_2 configuration in ~/gitmarks2.py.'
